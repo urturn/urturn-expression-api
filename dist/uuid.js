24b639ce269035265b9952f12671d8e309a65b0c
Olivier Amblet <olivier.amblet@webdoc.com>
2013-01-31 09:03:57 +0100

    Fixed uuid bug.

--- dist/uuid.js
+++ dist/uuid.js
@@ -8,7 +8,7 @@ var UT = {}
 // Fantastic piece of code from @broofa on:
 // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
 UT.uuid = function(){
-  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
   });
