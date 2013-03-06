67a6c1b5a25500276857b15522ad49ca515a2778
Olivier Amblet <olivier.amblet@webdoc.com>
2013-03-06 17:55:44 +0100

    Better testing

--- testExpression/testGlobal/test.js
+++ testExpression/testGlobal/test.js
@@ -6,33 +6,6 @@
  * same page so you should always limit your DOM queries to expression.getElement().
  */
 
-var Test = function(expression){
-  this.expression = expression;
-  this.imageWrapper = expression.getElement().querySelector('div.picture');
-  this.nameWrapper = expression.getElement().querySelector('span.name');
+var Test = function(post){
   
-  var self = this;
-  // display the image.
-  self.displayImage(this.expression.storage.getItem('picture'));
-  
-  // display the name.
-  if(self.nameWrapper){
-    self.nameWrapper.innerHTML = this.expression.storage.getItem('myName');
-  }
-  self.expression.container.autoResize();
-};
-
-Test.prototype.displayImage = function(imageResource) {
-  if(!imageResource){
-    return;
-  }
-  if (!this.img) {
-    this.img = document.createElement('img');
-    this.imageWrapper.appendChild(this.img);
-    var self = this;
-    this.img.addEventListener('load', function(event){
-      self.expression.container.autoResize();
-    });
-  }
-  this.img.src = imageResource.url;
 };
