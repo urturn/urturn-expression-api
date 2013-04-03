3f64d3c4ba70bcf481c45dab02f3a4e487c124c0
Olivier Amblet <olivier@amblet.net>
2013-04-03 16:44:41 +0200

    compiled iframe.js

--- dist/iframe.js
+++ dist/iframe.js
@@ -60,15 +60,16 @@ UT.Collection = function(options) {
   // PUBLIC Properties
   this.length = 0; // loded items count
   this.name = null;
-
-  // PUBLIC Methods
+  this.isPublic = function isPublic(){
+    return false;
+  };
 
   // Add or updated an item binded to a specific key
-  var setItem = function(key, item) {
+  var setItem = this.setItem = function(key, item) {
     if(!key) {
       throw new Error("InvalidKey", key);
     }
-    if(privateKeys.indexOf(key) != -1) {
+    if(UT.Collection.isReservedKey(key)) {
       throw new Error("ReservedKey", key);
     }
     var sanItem = sanitizeItem(key, item);
@@ -100,9 +101,8 @@ UT.Collection = function(options) {
     this.length = keys.length;
     return item;
   };
-  this.setItem = setItem;
 
-  var getItem = function(key, defaultValue) {
+  var getItem = this.getItem = function(key, defaultValue) {
     if(!key) {
       return null;
     }
@@ -119,27 +119,12 @@ UT.Collection = function(options) {
       return item;
     }
   };
-  this.getItem = getItem;
 
   this.key = function(n) {
     if(n >= keys.length) {
       return null;
     }
     return keys[n];
-  };
-
-  this.average = function(name) {
-    if(!operations[name]) {
-      return;
-    }
-    return operations[name].average;
-  };
-
-  this.sum = function(name) {
-    if(!operations[name]) {
-      return;
-    }
-    return operations[name].sum;
   };
 
   /**
@@ -149,16 +134,8 @@ UT.Collection = function(options) {
    * This last option is available only for field declared
    * in collection field.
    */
-  this.count = function(filterKey) {
-    if(!filterKey){
-      return count;
-    } else if(!operations[filterKey]) {
-      return;
-    } else {
-      if(operations[filterKey]){
-        return operations[filterKey].count;
-      }
-    }
+  this.count = function() {
+    return count;
   };
 
   this.toString = function() {
@@ -214,8 +191,7 @@ UT.Collection = function(options) {
 
   // PRIVATE Properties
   var currentUserId, delegate, data, items,
-      keys, dirtyKeys, boundKeys, count,
-      privateKeys = ['refresh', 'setItem', 'getItem', 'count', 'sum', 'key', 'getUserItem', 'setUserItem', 'average', 'toString', 'size', 'length', 'length', 'name', 'save', 'fieldDefs', 'sanitizedItem', 'getCurrentData'];
+      keys, dirtyKeys, boundKeys, count;
 
   // Cleanup item to keep only authorized keys
 
@@ -253,7 +229,7 @@ UT.Collection = function(options) {
   };
 
   var bindItem = function(key, value) {
-    if(boundKeys.indexOf(key) == -1 && privateKeys.indexOf(key) == -1){
+    if(boundKeys.indexOf(key) == -1 && !UT.Collection.isReservedKey(key)){
       boundKeys.push(key);
       this.__defineGetter__(key, function() {
         return getItem.call(this, key);
@@ -268,13 +244,37 @@ UT.Collection = function(options) {
     var keys = Object.keys(this);
     for(var i = keys.length - 1; i >= 0; i--) {
       var key = keys[i];
-      if(privateKeys.indexOf(key) == -1 && boundKeys.indexOf(key) == -1) {
+      if(!UT.Collection.isReservedKey(key) && boundKeys.indexOf(key) == -1) {
         setItem.call(this, key, this[key]);
         bindItem.call(this, key, this[key]);
       }
     }
   };
   initialize.call(this, options);
+};
+
+UT.Collection.RESERVED_KEYS = [
+  'refresh',
+  'setItem',
+  'getItem',
+  'count',
+  'sum',
+  'key',
+  'isPublic',
+  'getUserItem',
+  'setUserItem',
+  'average',
+  'toString',
+  'size',
+  'length',
+  'name',
+  'save',
+  'fieldDefs',
+  'sanitizedItem',
+  'getCurrentData'
+];
+UT.Collection.isReservedKey = function(key) {
+  return UT.Collection.RESERVED_KEYS.indexOf(key) >= 0;
 };
 
 UT.Collection.validateOptions = function(options){
@@ -342,12 +342,21 @@ UT.CollectionStore = function(options) {
     if(!name) {
       throw new Error("ArgumentError", "data contains unamed collections.");
     }
-    collections[name] = new UT.Collection({
-      data: data,
-      postMessageApi: options.postMessageApi,
-      currentUserId: options.currentUserId,
-      delegate: delegate
-    });
+    if(data.public){
+      collections[name] = new UT.PublicCollection({
+        data: data,
+        postMessageApi: options.postMessageApi,
+        currentUserId: options.currentUserId,
+        delegate: delegate
+      });
+    } else {
+      collections[name] = new UT.Collection({
+        data: data,
+        postMessageApi: options.postMessageApi,
+        currentUserId: options.currentUserId,
+        delegate: delegate
+      });
+    }
   }
 
   // Retrieve a collection given its name.
@@ -389,6 +398,9 @@ UT.CollectionStore = function(options) {
 
 ; (function(){
   "use strict";
+
+  var VALID_FILTERS = ['recent', 'friends'];
+
   /**
    * valid options keys: data, delegate, currentUserId
    */
@@ -399,6 +411,10 @@ UT.CollectionStore = function(options) {
 
     var userItem = null;
     var dirty = false;
+
+    this.isPublic = function isPublic(){
+      return true;
+    };
 
     // PUBLIC Methods
     // Add an anonymous item (without a key).
@@ -557,6 +573,31 @@ UT.CollectionStore = function(options) {
       return newData;
     };
 
+    /**
+     * Let you find the latest items.
+     * 
+     * @params [options] or a filter string
+     * @params callback function
+     */
+    this.find = function(options, callback) {
+      if(typeof options === 'function') {
+        callback = options;
+        options = {filters: {recent: true}};
+      }
+      if(typeof options === 'string') {
+        if(VALID_FILTERS.indexOf(options) === -1){
+          throw new Error('invalid filter ('+ options +')');
+        }
+        var filters = {};
+        filters[options] = true;
+        options = {filters: filters};
+      }
+      if(!callback || typeof callback !== 'function'){
+        throw new Error('missing callback argument');
+      }
+      delegate.find(this.name, options, callback);
+    };
+
     this.refresh = function(json) {
       initialize.call(this, {currentUserId: currentUserId, delegate: delegate, data: json});
     };
@@ -589,12 +630,10 @@ UT.CollectionStore = function(options) {
           if(oldItem && oldItem[operation.field] !== undefined) {
             field.average = (field.average * (count) - oldItem[operation.field]) / parseFloat(count - 1);
             count--;
-            console.log('Average: substract from old item:' + field.average);
           }
           if(newItem && newItem[operation.field] !== undefined) {
             field.average = (field.average * (count) + newItem[operation.field]) / parseFloat(count + 1);
             count++;
-            console.log('Average: add from new item:' + field.average);
           }
           field.average_count = count;
         }
@@ -692,7 +731,6 @@ UT.CollectionStore = function(options) {
 
     initialize.call(this, options);
   };
-
 })();
 
 ; (function(){
@@ -854,6 +892,9 @@ UT.CollectionStore = function(options) {
         },
         authenticate: function(callback) {
           UT.Expression._callAPI('authenticate');
+        },
+        find: function(name, options, callback) {
+          UT.Expression._callAPI('collections.find', [name, options], callback);
         }
       }
     });
