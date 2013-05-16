30af7fd1e8ceca5496c5dc9a6c60e30291e7d3f4
Olivier Amblet <olivier.amblet@webdoc.com>
2013-05-16 09:43:44 +0200

    Merged scroll spec(actually replaced them)

--- testExpression/bdd/specs/post-scroll-spec.js
+++ testExpression/bdd/specs/post-scroll-spec.js
@@ -1,21 +1,60 @@
 UT.Expression.ready(function(post){
-  describe('UT.Post.scroll()', function(){
-    it('should return scroll informations', function(){
-      var value = post.scroll();
-      expect(value).to.be.ok();
-      expect(value.scrollTop).to.eql(0);
-      expect(value.scrollBottom).to.be.greaterThan(0);
-    });
-    it('let you set a new size', function(done){
-      var cb = function(size){
-        var w = post.size().width;
-        expect(size.height).to.eql(233);
-        expect(size.width).to.eql(w);
-        post.off('resize', cb);
+  describe.only('UT.Post.scroll()', function(){
+    function resize(height, done){
+      var scope = {};
+      scope.fn = function(evt){
+        expect(scope.resized).to.be.ok();
+        post.off('scroll', scope.fn);
         done();
       };
-      post.on('resize', cb);
-      post.resize({height: 233});
+      post.size(height, function(event){
+        post.on('scroll', scope.fn);
+        scope.resized = true;
+      });
+    }
+
+    beforeEach(function(done){
+      resize(5, done);
+    });
+
+    it('retrieve scroll informations', function(done){
+      var result = post.scroll();
+      expect(result.scrollTop).to.eql(0);
+      expect(result.scrollBottom).to.eql(0);
+
+      resize(5000, function(){
+        result = post.scroll();
+        expect(result.scrollTop).to.eql(0);
+        expect(result.scrollBottom).to.be.greaterThan(0);
+        done();
+      });
+    });
+
+    it('pass the scroll informations to a callback and return the post', function(done){
+      var result = post.scroll(function(event){
+        expect(event.scrollTop).to.eql(0);
+        expect(event.scrollBottom).to.eql(0);
+
+        resize(5000, function(){
+          result = post.scroll(function(event){
+            expect(event.scrollTop).to.eql(0);
+            expect(event.scrollBottom).to.be.greaterThan(0);
+            done();
+          });
+          expect(result).to.be(post);
+        });
+      });
+      expect(result).to.be(post);
+    });
+
+    it('let you scroll to a specific position', function(done){
+      resize(5000, function(){
+        post.scroll({bottom:0}, function(event){
+          expect(event.scrollBottom).to.be(0);
+          expect(event.scrollTop).to.be.greaterThan(0);
+          done();
+        });
+      });
     });
   });
 });
