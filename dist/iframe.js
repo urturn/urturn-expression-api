ea8ef24faefe05c3b608773920280f94a6d4dbbe
Pierre-Loic Doulcet <pierre.doulcet@webdoc.com>
2014-04-24 15:22:21 +0200

    1.3.4-alpha33

--- dist/iframe.js
+++ dist/iframe.js
@@ -13002,7 +13002,7 @@ UT.CollectionStore = function(options) {
    * Retrieve the API version of the current expression
    */
   UT.Expression.apiVersion = function() {
-    return states && states.apiVersion || '1.3.4-alpha32';
+    return states && states.apiVersion || '1.3.4-alpha33';
   };
 
   UT.Expression.version = function() {
@@ -22358,7 +22358,8 @@ function loadCutOut() {
 
           that.backFromLib = function(data) {
             that.options.imageData = data;
-            methods.init(that.options);
+            that.options.fastQuit = true;
+            $that.init(that.options);
           };
           
           that.revertToLib = function() {
