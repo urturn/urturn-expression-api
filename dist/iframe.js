811cd11fc69278754366c66cf21eacd1808b1c50
Julien Bachmann <julien.bachmann@urturn.com>
2014-12-02 14:11:51 +0100

    1.3.5-alpha3-julien

--- dist/iframe.js
+++ dist/iframe.js
@@ -2575,7 +2575,7 @@ UT.CollectionStore = function(options) {
    * Retrieve the API version of the current expression
    */
   UT.Expression.apiVersion = function() {
-    return states && states.apiVersion || '1.3.5-alpha3';
+    return states && states.apiVersion || '1.3.5-alpha3-julien';
   };
 
   UT.Expression.version = function() {
